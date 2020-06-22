1a2629a3f287d90f5f34c6de7b2e1e1f3d7f4c01
Sushmith Thuluva <42782828+ST797@users.noreply.github.com>
2020-06-22 18:00:02 +0530

    Add files via upload

---
+++ weapons.js
@@ -0,0 +1,71 @@
+document.getElementById("hv").addEventListener("click",heat);
+function heat(){
+  document.getElementById("i").src = "heat vision.jpg";
+}
+
+document.getElementById("fb").addEventListener("click",freeze);
+function freeze(){
+  document.getElementById("i").src = "freezing breath.jpg";
+}
+
+document.getElementById("sp").addEventListener("click",punch);
+function punch(){
+  document.getElementById("i").src = "punch.jpg";
+}
+
+
+document.getElementById("ls").addEventListener("click",lasso);
+function lasso(){
+  document.getElementById("i").src = "ls.jpg";
+}
+
+document.getElementById("Bs").addEventListener("click",bracelet);
+function bracelet(){
+  document.getElementById("i").src = "Bracelet.jpg";
+}
+
+document.getElementById("SA").addEventListener("click",sword);
+function sword(){
+  document.getElementById("i").src = "sword.png";
+}
+
+document.getElementById("spunch").addEventListener("click",speedpunch);
+function speedpunch(){
+  document.getElementById("i").src = "spunch.jpg";
+}
+document.getElementById("smirage").addEventListener("click",speedmirage);
+function speedmirage(){
+  document.getElementById("i").src = "The Flash1.jpg";
+}
+document.getElementById("lbolt").addEventListener("click",lightning);
+function lightning(){
+  document.getElementById("i").src = "lbolt.jpg";
+}
+
+document.getElementById("fpower").addEventListener("click",fullpower);
+function fullpower(){
+  document.getElementById("i").src = "fpower.jpg";
+}
+
+document.getElementById("gshield").addEventListener("click",greens);
+function greens(){
+  document.getElementById("i").src = "gshield.jpg";
+}
+document.getElementById("any").addEventListener("click",anyweapon);
+function anyweapon(){
+  document.getElementById("i").src = "any.jpg";
+}
+
+document.getElementById("trident").addEventListener("click",atrident);
+function atrident(){
+  document.getElementById("i").src = "atrident.jpg";
+}
+document.getElementById("seac").addEventListener("click",sea);
+function sea(){
+  document.getElementById("i").src = "sea.jpg";
+}
+
+document.getElementById("aqua").addEventListener("click",aq);
+function aq(){
+  document.getElementById("i").src = "astrength.jpg";
+}
